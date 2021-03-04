c1f210921591bc036cf4a9183f2fcc965929f271
Pravin Kumar Jaisawal <udayapur.pravin@gmail.com>
2021-03-04 11:58:08 +0100

    Add QString example 5 substring

---
+++ strings/src/qstring5.cpp
@@ -0,0 +1,22 @@
+// using Qstring example4
+// Using substrings in QString 
+
+
+#include <QTextStream>
+
+int main(void) 
+{
+    QTextStream out(stdout);
+
+    QString str {"This is best"};
+    out << str.left(4) << endl; // from left
+    out << str.right(4) << endl; // from right
+    out << str.mid(5,2) << endl;  // from start to (start+len)
+
+    QString str2("The American Dream"); 
+    QStringRef sub(&str2, 4, 8); // This is a readonly string and works similar to mid
+
+    out << sub.toString() << endl;
+
+    return 0;
+}
