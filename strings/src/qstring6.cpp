aa6c50c90e05f6b28ab042b9461da2c133f07d8f
Pravin Kumar Jaisawal <udayapur.pravin@gmail.com>
2021-03-04 12:09:16 +0100

    Add looping in QString example

---
+++ strings/src/qstring6.cpp
@@ -0,0 +1,30 @@
+// using Qstring example4
+// Looping through QString. Remember QString consists of QChars
+
+
+#include <QTextStream>
+
+int main(void) 
+{
+    QTextStream out(stdout);
+
+    QString str {"There are many stars."};
+
+    // method 1
+    for (QChar qc : str) {
+        out << qc << " " ;
+    }
+    out << endl;
+
+    // method 2
+    for (int i = 0; i < str.count(); ++i)
+        out << str.at(i) << " ";
+    out << endl;
+
+    //method 3
+    for (QChar* it = str.begin(); it !=str.end(); ++it) 
+        out << *it << " ";
+    out << endl;
+
+    return 0; 
+}
