7d43ece339d054deb9f696d29f7bc05fb2c5f99a
Pravin Kumar Jaisawal <udayapur.pravin@gmail.com>
2021-03-04 13:09:26 +0100

    Aligning example with Qstring

---
+++ strings/src/qstring11.cpp
@@ -0,0 +1,24 @@
+// using Qstring example10
+// aligning strings, leftJustified(), rightJustified() methods
+
+#include <QTextStream>
+
+
+int main(void) 
+{
+    QTextStream out(stdout);
+
+    QString field1 { "Name " }; 
+    QString field2 { "Occupation " }; 
+    QString field3 { "Residence " }; 
+    QString field4 { "Marital status " }; 
+
+    int width = field4.size(); // using the size of field4 because it is longest
+
+    out << field1.leftJustified(width, ' ') << ":" << " Pravin\n";
+    out << field2.leftJustified(width, ' ') << ":" <<" Engineer\n";
+    out << field3.leftJustified(width, ' ') << ":" <<" Germany\n";
+    out << field4.leftJustified(width, ' ') << ":" <<" Single\n";
+
+    return 0;
+}
