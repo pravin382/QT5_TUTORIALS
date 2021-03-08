221531e74bec8b19b393773c79fbb751b3043f59
Pravin Kumar Jaisawal <udayapur.pravin@gmail.com>
2021-03-08 23:25:39 +0100

    Add isLeapYear() method

---
+++ date_and_time/src/leapyear.cpp
@@ -0,0 +1,21 @@
+// Program to check if it is leap year or not
+#include <QTextStream>
+#include <QDate>
+
+int main(void) {
+
+   QTextStream out(stdout);
+
+   QList<int> years({2010, 2011, 2012, 2013, 2014, 2015, 2016, 2020, 2024});
+
+   for (int year: years) {
+
+       if (QDate::isLeapYear(year)) { // isLeapYear()
+
+           out << year << " is a leap year" << endl;
+       } else {
+
+           out << year << " is not a leap year" << endl;
+       }
+   }
+}
