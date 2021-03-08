59534d168afa0582c91d1f29b9c9e18ba394e2c6
Pravin Kumar Jaisawal <udayapur.pravin@gmail.com>
2021-03-08 23:20:55 +0100

    Add compare program in qt

---
+++ date_and_time/src/compare_dates.cpp
@@ -0,0 +1,23 @@
+#include <QTextStream>
+#include <QDate>
+
+int main(void) {
+
+   QTextStream out(stdout);
+
+   // defining two dates
+   QDate dt1 { 2020, 4, 5 };
+   QDate dt2 { 2019, 4, 5 };
+
+   if (dt1 < dt2) {
+
+       out << dt1.toString() << " comes before "
+            << dt2.toString() << endl;
+   } else {
+
+       out << dt1.toString() << " comes after "
+            << dt2.toString() << endl;
+   }
+
+   return 0;
+}
